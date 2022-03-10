1e7644a6188ebe4f3728a6aa35d836da506927d6
Jo M <jonathanmaison78@gmail.com>
2022-03-10 06:50:55 +0100

    Made a fake loading time for home page

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import ButtonLink from '../Components/ButtonLink/ButtonLink';
+import LoadingScreen from '../Components/LoadingScreen/LoadingScreen';
 
 export default function Home({ screenWidth }) {
+    const [isLoading, setIsLoading] = useState(true);
+
+    function initLoadingTime() {
+        setTimeout(
+            () => setIsLoading(false), 300
+        )
+    };
+    useEffect(() => {
+        initLoadingTime();
+        clearTimeout(initLoadingTime);
+    }, []);
+
     return (
         <main>
             <div className='hero'>
                 <h1>Ox4Me</h1>
-                {/* <img src={Logo} alt="Logo de l'Oxford Pub" className="container" /> */}
             </div>
-            <section className={`home-list ${screenWidth >= 768 ? "container" : ''}`}>
-                <ButtonLink isLink variant='home-link' to='list' content='La liste' />
-                <ButtonLink isLink variant='home-link' to='random' content='Cocktail aléatoire' />
-                <ButtonLink isLink variant='home-link' to='favorites' content='Vos favoris' />
-            </section>
+            {
+                isLoading ? <LoadingScreen /> :
+                    <section className={`home-list ${screenWidth >= 768 ? "container" : ''}`}>
+                        <ButtonLink isLink variant='home-link' to='list' content='La liste' />
+                        <ButtonLink isLink variant='home-link' to='random' content='Cocktail aléatoire' />
+                        <ButtonLink isLink variant='home-link' to='favorites' content='Vos favoris' />
+                    </section>
+            }
         </main>
     )
 }
