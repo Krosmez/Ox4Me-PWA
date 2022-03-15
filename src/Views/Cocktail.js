764013c398bf6dc4caca3c6a1b9f0a4cf42c1c1a
Jo M <jonathanmaison78@gmail.com>
2022-03-15 21:00:56 +0100

    Rework Cocktail View

--- src/Views/Cocktail.js
+++ src/Views/Cocktail.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import OxAPI from '../data/OxAPI';
-import StorageTools from "../data/StorageTools";
-import IngredientListItem from '../Components/IngredientsListItem/IngredientListItem';
+import StorageTools from '../data/StorageTools';
 import LoadingScreen from '../Components/LoadingScreen/LoadingScreen';
-import { ReactComponent as CheckBox } from '../img/check-unboxed.svg'
-import { ReactComponent as Cross } from '../img/crossed.svg'
-import { ReactComponent as Heart } from '../img/heart.svg'
-import { ReactComponent as HeartFill } from '../img/heart-fill.svg'
+import IconBar from '../Components/IconBar/IconBar';
+import IngredientListItem from '../Components/IngredientsListItem/IngredientListItem';
 
 export default function Cocktail() {
     const params = useParams();
@@ -52,11 +49,11 @@ export default function Cocktail() {
     } else {
         return (
             <main className='cocktail-details container'>
+
                 <section className='cocktail-top'>
+
                     <div className='cocktail-info'>
-                        <div>
-                            <img src={`https://ox4me.herokuapp.com/static/images/drink/${params.id}.svg`} alt={`Image du cocktail ${drinkName}`} loading='lazy'/>
-                        </div>
+
                         <div>
                             <h2>{drinkName}</h2>
                             <p>
@@ -69,19 +66,20 @@ export default function Cocktail() {
                                 }
                             </p>
                         </div>
+
+                        <IconBar
+                            isConsumed={isConsumed}
+                            setResetConsumed={setResetConsumed}
+                            addRemoveLike={addRemoveLike}
+                            isLike={isLike}
+                        />
+
                     </div>
 
-                    <div className='cocktail-icons'>
-                        <label htmlFor='already-drink' onClick={setResetConsumed} className={!isConsumed ? 'btn-consumed ' : 'btn-consumed checked'}>
-                            <input type='checkbox' name='already-drink' checked={isConsumed} readOnly />
-                            {!isConsumed ? <Cross /> : <CheckBox />}
-                            {!isConsumed ? " Jamais testé" : " Déjà bû"}
-                        </label>
-                        <label htmlFor='like' onClick={addRemoveLike} className='btn-heart'>
-                            <input type='checkbox' name='like' checked={isLike} readOnly />
-                            {!isLike ? <Heart /> : <HeartFill />}
-                        </label>
+                    <div className='cocktail-img'>
+                        <img src={`https://ox4me.herokuapp.com/static/images/drink/${params.id}.svg`} alt={`Image du cocktail ${drinkName}`} loading='lazy' />
                     </div>
+
                 </section>
 
                 <section className='cocktail-bottom'>
